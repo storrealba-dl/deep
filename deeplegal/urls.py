e63f35fa8edfbdc5f740d9837ceeb5302aecdfc5
Django Server <joropito@gmail.com>
2019-01-07 19:45:04 +0000

    models: url handling moved to managers

--- deeplegal/urls.py
+++ deeplegal/urls.py
@@ -1,18 +1,5 @@
-from django.urls import path
-from managers.views import *
+from django.urls import path, include
 
 urlpatterns = [
-#    path('admin/', admin.site.urls),
-  path('users/', UsersView.as_view(), name='test'),
-  path('users/<int:id>/', UsersView.as_view(), name='test'),
-  path('menus/', MenusView.as_view(), name='test'),
-  path('menus/<int:id>/', MenusView.as_view(), name='test'),
-  path('menusitems/', MenusItemsView.as_view(), name='test'),
-  path('menusitems/<int:id>/', MenusItemsView.as_view(), name='test'),
-  path('companies/', CompaniesView.as_view(), name='test'),
-  path('companies/<int:id>/', CompaniesView.as_view(), name='test'),
-  path('companies/<int:company_id>/ruts/', RutsView.as_view(), name='test'),
-  path('companies/<int:company_id>/ruts/<int:id>/', RutsView.as_view(), name='test'),
-  path('companies/<int:company_id>/teams/', TeamsView.as_view(), name='test'),
-  path('companies/<int:company_id>/teams/<int:id>/', TeamsView.as_view(), name='test'),
+  path('', include('managers.urls')),
 ]
