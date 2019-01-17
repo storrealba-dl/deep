c48cc6073acb9111dff1773fd1aabcfc1d5a0849
Django Server <joropito@gmail.com>
2019-01-17 02:06:16 +0000

    added urls for users,companies,ruts,teams,menus

--- admin/urls.py
+++ admin/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'admin'
 
 urlpatterns = [
-    path('/users/', views.users, name='users'),
-    path('/companies/', views.companies, name='companies'),
-    path('/teams/', views.teams, name='teams'),
-    path('/ruts/', views.ruts, name='ruts'),
-    path('/menus/', views.menus, name='menus'),
+    path('users/', views.users, name='users'),
+    path('companies/', views.companies, name='companies'),
+    path('teams/', views.teams, name='teams'),
+    path('ruts/', views.ruts, name='ruts'),
+    path('menus/', views.menus, name='menus'),
 ]
 
 
